55d7247ec9d9d891822ef0a382234489abdb7093
Kristopher Hill <Kristopher2.Hill@live.uwe.ac.uk>
2018-09-27 04:01:22 +0100

    Reversable gravity for player ball

---
+++ Assets/Scripts/PlayerGravity.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerGravity : MonoBehaviour
+{
+    [SerializeField] private bool m_reverse_gravity = false;
+    // Use this for initialization
+
+
+    void Start()
+    {
+        if (m_reverse_gravity)
+        {
+            var rigidbody = this.GetComponent<Rigidbody2D>();
+            if (!rigidbody)
+            {
+                Debug.Log("No Rigidbody on " + this.name);
+                return;
+            }
+
+            rigidbody.gravityScale *= -1;
+        }
+    }
+
+    private void Update()
+    {
+        if (m_reverse_gravity)
+        {
+            var rigidbody = this.GetComponent<Rigidbody2D>();
+            if (!rigidbody)
+            {
+                Debug.Log("No Rigidbody on " + this.name);
+                return;
+            }
+
+            rigidbody.gravityScale = -1;
+        }
+        else
+        {
+            var rigidbody = this.GetComponent<Rigidbody2D>();
+            if (!rigidbody)
+            {
+                Debug.Log("No Rigidbody on " + this.name);
+                return;
+            }
+
+            rigidbody.gravityScale = 1;
+        }
+    }
+}
